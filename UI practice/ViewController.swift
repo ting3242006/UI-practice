4fce360d9bff7997764c0e1fedc20741b9653c4b
ting3242006 <ting3242006@hotmail.com>
2021-07-14 12:52:33 +0800

    Initial Commit

---
+++ UI practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UI practice
+//
+//  Created by Ting on 2021/7/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
